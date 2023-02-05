b26f63f840f3a70891ecf6da84d47d190698dc60
sayraarriaga <sayraarriaga4@gmail.com>
2023-02-05 01:30:03 -0600

    feedback changes

--- scripts/personalportal.js
+++ scripts/personalportal.js
@@ -2,3 +2,5 @@ const date = new Date().toDateString();
 
 document.querySelector("#dateholder").textContent = date;
 
+const currenyYear = new Date;
+document.querySelector("#year").textContent = currenyYear.getFullYear();
