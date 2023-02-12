6ff290e4f2acb8577c92f1ed3be8afc24e30d169
sayraarriaga <sayraarriaga4@gmail.com>
2023-02-11 22:46:07 -0600

    chamber changes week 06

---
+++ lesson4/chamber/scripts/windchill.js
@@ -0,0 +1,16 @@
+function windChill(number1, number2){
+    let wc = 35.74 + 0.6215 * number1 - 35.75 * Math.pow(number2, 0.16) + 0.4275 * number1 * Math.pow(number2, 0.16);
+    return wc
+}
+
+function getWindChill(){
+    let temperature = parseInt(document.querySelector('#temperature').value);
+    let windSpeed= parseInt(document.querySelector('#windspeed').value);
+    let result = windChill(temperature, windSpeed);
+    result = Math.floor(result);
+    document.querySelector('#windchill').value = result;
+}
+
+document.querySelector('#calculate').addEventListener('click', getWindChill);
+
+    
