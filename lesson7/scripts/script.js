7713f05c279a653d56df4f624805f793484ec7fc
sayraarriaga <sayraarriaga4@gmail.com>
2023-02-23 01:52:15 -0600

    lazy Load

---
+++ lesson7/scripts/script.js
@@ -0,0 +1,30 @@
+const images= document.querySelectorAll("[data-src]");
+
+function preloadImage(img){
+    const src = img.getAttribute("data-src");
+    if(!src){
+        return;
+    }
+    img.src = src;
+}
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const imageObserver = new IntersectionObserver((entries, imageObserver) =>{
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return;
+        }else{
+            preloadImage(entry.target);
+            imageObserver.unobserve(entry.target);
+        }
+        
+    });
+
+}, imgOptions);
+
+images.forEach(image =>{
+    imageObserver.observe(image);
+})
