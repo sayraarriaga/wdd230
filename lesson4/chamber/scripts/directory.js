4370d50c110e22941b0c2d84cdceb4d5c4f95262
sayraarriaga <sayraarriaga4@gmail.com>
2023-03-03 01:29:29 -0600

    json img

--- lesson4/chamber/scripts/directory.js
+++ lesson4/chamber/scripts/directory.js
@@ -12,7 +12,7 @@ async function getCompaniesData(url) {
   const displayCompanies = (companies) => {
     const cards = document.querySelector('div.cards'); // select the output container element
   
-    companies.forEach((company) => {
+    companies.forEach((companie) => {
       // Create elements to add to the div.cards element
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
@@ -20,23 +20,24 @@ async function getCompaniesData(url) {
       let phone = document.createElement('p');
       let websiteurl = document.createElement('a');
       let membershipl = document.createElement('p')
+      let logo = document.createElement('img');
   
       // Build the h2 content out to show the prophet's full name - finish the template string
-      h2.textContent = `${company.name}`;
-      adress.textContent = ` ${company.adress}`;
-      phone.textContent = `${company.phone}`;
-      websiteurl.textContent = `${company.websiteurl}`;
-      membershipl.textContent = `${company.membershipl}`;
+      h2.textContent = `${companie.name}`;
+      adress.textContent = ` ${companie.adresses}`;
+      phone.textContent = `${companie.phone}`;
+      websiteurl.textContent = `${companie.websiteurl}`;
+      membershipl.textContent = `${companie.membershipl}`;
       
 
       
   
       // Build the image portrait by setting all the relevant attribute
-      portrait.setAttribute('src', company.imagefile);
-      portrait.setAttribute('alt', `Portait of ${company.name}`);
-      portrait.setAttribute('loading', 'lazy');
-      portrait.setAttribute('width', '340');
-      portrait.setAttribute('height', '440');
+      logo.setAttribute('src', companie.imagefile);
+      logo.setAttribute('alt', `Portait of ${companie.name}`);
+      logo.setAttribute('loading', 'lazy');
+      logo.setAttribute('width', '340');
+      logo.setAttribute('height', '440');
   
       // Append the section(card) with the created elements
       card.appendChild(h2);
