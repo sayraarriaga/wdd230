7cbc2b24a165874f76608c2546f2512d263154af
sayraarriaga <sayraarriaga4@gmail.com>
2023-03-11 01:48:42 -0600

    chamber weatherapi

--- lesson4/chamber/scripts/windchill.js
+++ lesson4/chamber/scripts/windchill.js
@@ -1,16 +1,55 @@
-function windChill(number1, number2){
-    let wc = 35.74 + 0.6215 * number1 - 35.75 * Math.pow(number2, 0.16) + 0.4275 * number1 * Math.pow(number2, 0.16);
-    return wc
-}
 
-function getWindChill(){
-    let temperature = parseInt(document.querySelector('#temperature').value);
-    let windSpeed= parseInt(document.querySelector('#windspeed').value);
-    let result = windChill(temperature, windSpeed);
-    result = Math.floor(result);
-    document.querySelector('#windchill').value = result;
-}
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const windSpeed = document.querySelector('#windspeed');
+const windChill = document.querySelector('#windchill');
 
-document.querySelector('#calculate').addEventListener('click', getWindChill);
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Matamoros&units=imperial&wind.speed=miles/hour&main.feels_like=imperial&appid=03b7cea2d3e57d537934a6522281b505';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+       console.log(data); // this is for testing the call
+         displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+}
+  
+  apiFetch(url);
+
+  function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    windSpeed.innerHTML=  `${weatherData.wind.speed}`;
+    if (currentTemp < 50){
+    windChill.innerHTML= `${weatherData.main.feels_like}`
+    }
+    else{
+      windChill.innerHTML= "N/A";
+
+    }
+    
+  
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
+
+
+//function windChill(number1, number2){
+    //let wc = 35.74 + 0.6215 * number1 - 35.75 * Math.pow(number2, 0.16) + 0.4275 * number1 * Math.pow(number2, 0.16);
+    //return wc
+//}
 
     
